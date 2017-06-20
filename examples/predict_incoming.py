71c677f2594bd48bf0fc49007bc67bb7ebe55ee3
Manish Devgan <manish.nsit8@gmail.com>
2017-06-20 20:41:02 +0530

    added examples

---
+++ examples/predict_incoming.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('..//')
+
+from nsitbot import NSITBot
+
+"""
+very first example using NSITBot Module.
+in this example a test Bot is created with the 
+parameters / **kwargs and the name, version and
+the owner are printed out
+"""
+
+bot = NSITBot(
+    name="My Bot",
+    owner="Manish Devgan",
+    version="0.0.1"
+)
+
+"""
+Bot created above.
+
+__name__ = "My Bot"
+__owner__ = "Manish Devgan"
+__version__ = "0.0.1"
+"""
+
+"""
+printing the bot's characteristics
+"""
+
+print "Bot Name: ", bot.__name__
+
+print "Bot Verrsion: ", bot.__version__
+
+print "Bot Owner: ", bot.__owner__
